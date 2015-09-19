59d1b2820bc14e601d1769e5c1620409341aa518
Yonas Kbrom <kbromyonas@berkeley.edu>
2015-09-19 01:28:47 +0000

    hw1-1

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,10 @@
 class Foobar
   # Q4 CODE HERE
+	def baz(a)
+		y = []
+		a.map!{|x| x.to_i + 2}
+		y = a.select {|x| x%2 == 0}
+		return y
+	end
+	return baz()
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,18 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+	return (a+b) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+	a.sort!
+	a.map! {|x| x+1}
+	return a
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+	return first_name + " " + last_name
 end
 
 def blockin_time(a)

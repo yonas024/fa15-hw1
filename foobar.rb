cfd609493d0b8a97baf2b732ec9c14059aa0f310
Yonas Kbrom <kbromyonas@berkeley.edu>
2015-09-19 05:10:25 +0000

    hw1-2

--- foobar.rb
+++ foobar.rb
@@ -1,10 +1,11 @@
 class Foobar
   # Q4 CODE HERE
-	def baz(a)
+	def self.baz(a)
 		y = []
 		a.map!{|x| x.to_i + 2}
 		y = a.select {|x| x%2 == 0}
+		y = y.keep_if {|x| x <= 10}
+		y = y.inject {|sum,x| sum + x}
 		return y
 	end
-	return baz()
 end
